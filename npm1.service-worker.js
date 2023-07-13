c1d264807721d029c423f626a2c81a838858ccb2
yulei <yulei@yinhai.com>
2023-07-13 16:01:05 +0800

    fix: 调整路径匹配前缀

--- npm1.service-worker.js
+++ npm1.service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("fetch", function (event) {
-  if (event.request.url.startsWith("http://npm1.yinhaiyun.com/repository")) {
+  if (event.request.url.startsWith("https://npm1.yinhaiyun.com/repository")) {
     const redirectURL = event.request.url.replace(
-      "http://npm1.yinhaiyun.com/repository",
+      "https://npm1.yinhaiyun.com/repository",
       "https://lc.yinhaiyun.com/yh-material-node"
     );
     event.respondWith(fetch(new Request(redirectURL)));
